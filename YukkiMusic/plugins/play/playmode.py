604800424a54605c6bb4410f0d4a48e6c3c8f3be
SHHRAM <159306415+uuuu88@users.noreply.github.com>
2024-12-16 13:12:57 +0200

    Add files via upload

---
+++ YukkiMusic/plugins/play/playmode.py
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, Message
+from strings.filters import command
+from config import BANNED_USERS
+from YukkiMusic import app
+from YukkiMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from YukkiMusic.utils.decorators import language
+from YukkiMusic.utils.inline.settings import playmode_users_markup
+
+### Commands
+@app.on_message(
+    command(["مود التشغيل"]) & ~BANNED_USERS)
+@language
+async def playmode_(client, message: Message, _):
+    playmode = await get_playmode(message.chat.id)
+    if playmode == "المشرفين فقط":
+        Direct = True
+    else:
+        Direct = None
+    is_non_admin = await is_nonadmin_chat(message.chat.id)
+    if not is_non_admin:
+        Group = True
+    else:
+        Group = None
+    playty = await get_playtype(message.chat.id)
+    if playty == "الجميع":
+        Playtype = None
+    else:
+        Playtype = True
+    buttons = playmode_users_markup(_, Direct, Group, Playtype)
+    response = await message.reply_text(
+        _["playmode_1"].format(message.chat.title),
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )

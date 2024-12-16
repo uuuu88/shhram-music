37139f9dd4a2a61eed891ccab9f651e3689cc97e
SHHRAM <159306415+uuuu88@users.noreply.github.com>
2024-12-16 13:41:00 +0200

    Add files via upload

---
+++ YukkiMusic/__main__.py
@@ -0,0 +1,72 @@
+# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+import asyncio
+import importlib
+from sys import argv
+
+from pyrogram import idle
+
+import config
+from config import BANNED_USERS
+from YukkiMusic import HELPABLE, LOGGER, app, telethn, userbot
+from YukkiMusic.core.call import Yukki
+from YukkiMusic.plugins import ALL_MODULES
+from YukkiMusic.utils.database import get_banned_users, get_gbanned
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("YukkiMusic").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
+        LOGGER("YukkiMusic").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except Exception:
+        pass
+
+    await app.start()
+    # await userbot.start()
+    for all_module in ALL_MODULES:
+        imported_module = importlib.import_module(f"YukkiMusic.plugins" + all_module)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+
+    LOGGER("YukkiMusic.plugins").info("تم تحميل ملفات فولدر . ")
+    await userbot.start()
+    await Yukki.start()
+    await Yukki.decorators()
+    LOGGER("YukkiMusic").info("بدء تشغيل البوت .")
+    await idle()
+    if len(argv) not in (1, 3, 4):
+        await telethn.disconnect()
+    else:
+        await telethn.run_until_disconnected()
+
+
+if __name__ == "__main__":
+    telethn.start(bot_token=config.BOT_TOKEN)
+    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
+    LOGGER("YukkiMusic").info("باي")
